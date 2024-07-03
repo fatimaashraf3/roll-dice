add50cd9f03b0451ced2095289ea1287a54ff4f6
fatimaaahraf <143636229+fatimaaahraf@users.noreply.github.com>
2024-07-03 22:03:22 +0500

    push

---
+++ script.js
@@ -0,0 +1,29 @@
+function dice(){
+    var x = Math.floor(Math.random()*6+1);
+var img1;
+switch(x)
+{
+    case 1:
+        img1 = document.getElementById("img").src = "src/img1.png"
+        break;
+
+    case 2:
+        img1 = document.getElementById("img").src = "src/img2.png"
+        break;
+
+    case 3:
+        img1 = document.getElementById("img").src = "src/img3.png"
+        break;
+
+    case 4:
+        img1 = document.getElementById("img").src = "src/img4.png"
+        break;
+
+    case 5:
+        img1 = document.getElementById("img").src = "src/img5.png"
+        break;
+
+    case 6:
+        img1 = document.getElementById("img").src = "src/img6.png"
+        break;
+}}
